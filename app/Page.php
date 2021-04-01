8e543648aa298332d9191e9de6957580de839bc7
katuo-sakana <katuo.webkaihatsu@gmail.com>
2021-04-02 07:31:49 +0900

    記事モデルからタグモデルへのリレーションの作成

--- app/Page.php
+++ app/Page.php
@@ -3,8 +3,13 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Page extends Model
 {
-    //
+    public function tags(): BelongsToMany
+    {
+        return $this->belongsToMany('App\Tag', 'page_tag')->withTimestamps();
+    }
 }
