64eb1f0f94b4ef63c43e86fe070dac1c69b2c79e
katuo-sakana <katuo.webkaihatsu@gmail.com>
2021-04-14 07:50:51 +0900

    bladeフォーマット変更

--- resources/views/page_list.blade.php
+++ resources/views/page_list.blade.php
@@ -1,40 +1,40 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+  <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
+      <div class="col-md-8">
+        <div class="card">
+          <div class="card-header">Dashboard</div>
 
-                <div class="card-body">
-                    <a class="btn btn-primary" href="{{ route('page_register') }}">新規ページをメモする</a>
-                    <div class="list-box-wrap">
-                        @foreach ($pages as $page)
-                        <div class="list-box">
-                            <div class="list-box__image">
-                                @if (!empty($page->site_image))
-                                <img src="{{ asset('storage/' . $page->site_image) }}">
-                                @else
-                                <img id="image" src="https://placehold.jp/200x200.png">
-                                @endif
-                            </div>
-                            <div class="list-box__content">
-                                @foreach ($page->tags as $tag)
-                                <a href="">
-                                    {{$tag->name}}
-                                </a>
-                                @endforeach
-                                <h5 class="list-box__title">
-                                    <a href="{{route('page_show', ['pageid' => $page->id])}}">{{$page->title}}</a>
-                                </h5>
-                            </div>
-                        </div>
-                        @endforeach
-                    </div>
+          <div class="card-body">
+            <a class="btn btn-primary" href="{{ route('page_register') }}">新規ページをメモする</a>
+            <div class="list-box-wrap">
+              @foreach ($pages as $page)
+                <div class="list-box">
+                  <div class="list-box__image">
+                    @if (!empty($page->site_image))
+                      <img src="{{ asset('storage/' . $page->site_image) }}">
+                    @else
+                      <img id="image" src="https://placehold.jp/200x200.png">
+                    @endif
+                  </div>
+                  <div class="list-box__content">
+                    @foreach ($page->tags as $tag)
+                      <a href="">
+                        {{ $tag->name }}
+                      </a>
+                    @endforeach
+                    <h5 class="list-box__title">
+                      <a href="{{ route('page_show', ['pageid' => $page->id]) }}">{{ $page->title }}</a>
+                    </h5>
+                  </div>
                 </div>
+              @endforeach
             </div>
+          </div>
         </div>
+      </div>
     </div>
-</div>
+  </div>
 @endsection
--- resources/views/page_show.blade.php
+++ resources/views/page_show.blade.php
@@ -1,25 +1,25 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+  <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{$page->title}}</div>
+      <div class="col-md-8">
+        <div class="card">
+          <div class="card-header">{{ $page->title }}</div>
 
-                <div class="card-body">
-                    @if (!empty($page->site_image))
-                    <img src="{{ asset('storage/' . $page->site_image) }}">
-                    @endif
-                    @if (!empty($page->site_url))
-                    　　<a href="{{$page->site_url}}" target="_blank">{{$page->site_url}}</a>
-                    @endif
-                    @if (!empty($page->comment))
-                    　　<div href="{{$page->comment}}">{{$page->comment}}</div>
-                    @endif
-                </div>
-            </div>
+          <div class="card-body">
+            @if (!empty($page->site_image))
+              <img src="{{ asset('storage/' . $page->site_image) }}">
+            @endif
+            @if (!empty($page->site_url))
+              <a href="{{ $page->site_url }}" target="_blank">{{ $page->site_url }}</a>
+            @endif
+            @if (!empty($page->comment))
+              <div href="{{ $page->comment }}">{{ $page->comment }}</div>
+            @endif
+          </div>
         </div>
+      </div>
     </div>
-</div>
+  </div>
 @endsection
