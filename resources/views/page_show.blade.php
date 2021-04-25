514f4648b61d9b25bb3240471a9e6242becfedfa
katuo-sakana <katuo.webkaihatsu@gmail.com>
2021-04-25 22:35:15 +0900

    詳細ページスタイル調整 #3

--- resources/views/page_show.blade.php
+++ resources/views/page_show.blade.php
@@ -3,23 +3,40 @@
 @section('content')
   <div class="width-base">
     <div class="card">
-      <div class="card-header">{{ $page->title }}</div>
-      <a class="btn btn-primary" href="{{ route('page_edit', [$page->id]) }}">編集する</a>
       <div class="card-body">
-        @if (!empty($page->site_image))
-          <img src="{{ asset('storage/' . $page->site_image) }}">
-        @endif
-        @foreach ($page->tags as $tag)
-          <a href="">
-            {{ $tag->hashtag }}
-          </a>
-        @endforeach
-        @if (!empty($page->site_url))
-          <a href="{{ $page->site_url }}" target="_blank">{{ $page->site_url }}</a>
-        @endif
-        @if (!empty($page->comment))
-          <div href="{{ $page->comment }}">{{ $page->comment }}</div>
-        @endif
+        <div class="page-navigation">
+          <a class="btn btn-primary page-navigation__item" href="{{ route('home') }}">一覧へ戻る</a>
+          <a class="btn btn-primary page-navigation__item" href="{{ route('page_edit', [$page->id]) }}">編集する</a>
+          <a class="btn btn-primary page-navigation__item" href="{{ route('page_register') }}">新規ページをメモする</a>
+        </div>
+        <div class="show-content">
+          <div class="show-content__primary">
+            <h1>{{ $page->title }}</h1>
+            @if (!empty($page->site_image))
+              <img src="{{ asset('storage/' . $page->site_image) }}">
+            @endif
+            <div class="tag-wrap">
+              @foreach ($page->tags as $tag)
+                <a class="tag" href="{{ route('tags_list', ['tag_name' => $tag->name]) }}">
+                  {{ $tag->hashtag }}
+                </a>
+              @endforeach
+            </div>
+            @if (!empty($page->site_url))
+              <div class="site-url">
+                <p class="site-url__title">サイトURL</p>
+                <a class="site-url__link" href="{{ $page->site_url }}" target="_blank">{{ $page->site_url }}</a>
+              </div>
+            @endif
+            @if (!empty($page->comment))
+              <div href="{{ $page->comment }}">{{ $page->comment }}</div>
+            @endif
+          </div>
+          <div class="show-content__secondary">
+            <h2>関連ページ</h2>
+          </div>
+        </div>
+
       </div>
     </div>
   </div>
