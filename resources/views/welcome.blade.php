a15c6067fa72f382258784b23b283b654e292812
katuo-sakana <katuo.webkaihatsu@gmail.com>
2021-05-04 22:23:27 +0900

    ロゴ設定

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,7 +8,7 @@
     <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
       <div class="container">
         <a class="navbar-brand" href="{{ url('/') }}">
-          {{ config('app.name', 'Laravel') }}
+          <img src="{{ asset('images/logo.png') }}" width="200" height="52">
         </a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
           aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
